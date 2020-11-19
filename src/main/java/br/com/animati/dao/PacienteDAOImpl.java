589dc959a26b12124878997d1e2edc8b97a91c16
Kadu Lemos <kadulemos@hotmail.com>
2020-11-19 15:02:15 -0300

    PacienteDAO

--- src/main/java/br/com/animati/dao/PacienteDAOImpl.java
+++ src/main/java/br/com/animati/dao/PacienteDAOImpl.java
@@ -1,44 +1,78 @@
 package br.com.animati.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.animati.entity.Paciente;
 
 public class PacienteDAOImpl implements PacienteDAO {
 
+	private List<Paciente> listaPacientes;
+
+	public PacienteDAOImpl() {
+		listaPacientes = new ArrayList<Paciente>();
+	}
+
 	@Override
 	public void cadastrar(Paciente p) {
-		// TODO Auto-generated method stub
+		listaPacientes.add(p);
 
 	}
 
 	@Override
 	public List<Paciente> list() {
-		// TODO Auto-generated method stub
-		return null;
+		return listaPacientes;
 	}
 
 	@Override
 	public void editar(Paciente p) {
-		// TODO Auto-generated method stub
+		boolean encontrar = true;
+		int indice = 0;
+
+		do {
+			if(listaPacientes.get(indice).getIdPaciente() == p.getIdPaciente()) {
+				listaPacientes.set(indice, p);
+				encontrar = false;
+			}
+			indice++;
+		} while (encontrar);
 
 	}
 
 	@Override
 	public void deletar(long idPaciente) {
-		// TODO Auto-generated method stub
+		boolean encontrar = true;
+		int indice = 0;
+
+		do {
+			if(listaPacientes.get(indice).getIdPaciente() == idPaciente) {
+				listaPacientes.remove(indice);
+				encontrar = false;
+			}
+			indice++;
+		} while (encontrar);
 
 	}
 
 	@Override
 	public Paciente listarPeloId(long idPaciente) {
-		// TODO Auto-generated method stub
-		return null;
+		Paciente pacienteEncontrado = null;
+		boolean encontrar = true;
+		int indice = 0;
+		
+		do {
+			if(listaPacientes.get(indice).getIdPaciente() == idPaciente) {
+				pacienteEncontrado = listaPacientes.get(indice);
+				encontrar = false;
+			}
+			indice++;
+		} while(encontrar);
+		return pacienteEncontrado;
 	}
 
 	@Override
 	public void limparLista() {
-		// TODO Auto-generated method stub
+		listaPacientes.clear();
 
 	}
 
--- src/main/java/br/com/animati/service/PacienteServiceImpl.java
+++ src/main/java/br/com/animati/service/PacienteServiceImpl.java
@@ -2,43 +2,49 @@ package br.com.animati.service;
 
 import java.util.List;
 
+import br.com.animati.dao.PacienteDAO;
+import br.com.animati.dao.PacienteDAOImpl;
 import br.com.animati.entity.Paciente;
 
 public class PacienteServiceImpl implements PacienteService {
+	
+	private PacienteDAO dao;
+	
+	public PacienteServiceImpl() {
+		dao = new PacienteDAOImpl();
+	}
 
 	@Override
 	public void cadastrar(Paciente p) {
-		// TODO Auto-generated method stub
+		dao.cadastrar(p);
 
 	}
 
 	@Override
 	public List<Paciente> list() {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.list();
 	}
 
 	@Override
 	public void editar(Paciente p) {
-		// TODO Auto-generated method stub
+		dao.editar(p);
 
 	}
 
 	@Override
 	public void deletar(long idPaciente) {
-		// TODO Auto-generated method stub
+		dao.deletar(idPaciente);
 
 	}
 
 	@Override
 	public Paciente listarPeloId(long idPaciente) {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.listarPeloId(idPaciente);
 	}
 
 	@Override
 	public void limparLista() {
-		// TODO Auto-generated method stub
+		dao.limparLista();
 
 	}
 
