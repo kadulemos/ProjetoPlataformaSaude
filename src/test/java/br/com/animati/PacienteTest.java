6ec93c6929b42cb55798a156f2000c45fecb1005
Kadu Lemos <kadulemos@hotmail.com>
2020-11-20 10:55:19 -0300

    Update PacienteTest

--- src/test/java/br/com/animati/PacienteTest.java
+++ src/test/java/br/com/animati/PacienteTest.java
@@ -1,9 +1,11 @@
 package br.com.animati;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -125,6 +127,50 @@ public class PacienteTest {
 		assertEquals("João da Silva", pacienteEditado.getNome());
 	}
 	
+	@Test
+	public void deletarTest() {
+		
+		pacienteService.limparLista();
+		
+		Paciente paciente = new Paciente();
+		paciente.setIdPaciente(12345);
+		paciente.setEmpresa("LG");
+		paciente.setNome("José da Silva");
+		paciente.setNomeSocial("");
+		paciente.setNomeMae("Laura da Silva");
+		paciente.setUf(UfType.SP);
+		paciente.setCpf("123.456.789-0");
+		paciente.setPassword("1111");
+		paciente.setRg("1.234.567-8");
+		paciente.setSexo(SexType.M);
+
+		pacienteService.cadastrar(paciente);
+		
+		pacienteService.deletar(12345);
+		
+		Assert.assertTrue(pacienteService.list().isEmpty());
+	}
 	
+	@Test
+	public void listarPeloIdTest() {
+		
+		pacienteService.limparLista();
+		
+		Paciente paciente = new Paciente();
+		paciente.setIdPaciente(54321);
+		paciente.setEmpresa("Sony");
+		paciente.setNome("Julia da Silva");
+		paciente.setNomeSocial("");
+		paciente.setNomeMae("Joana da Silva");
+		paciente.setUf(UfType.MG);
+		paciente.setCpf("908.765.432-1");
+		paciente.setPassword("2222");
+		paciente.setRg("9.897.432-4");
+		paciente.setSexo(SexType.F);
+		
+		pacienteService.cadastrar(paciente);
+		
+		assertNotNull(pacienteService.listarPeloId(54321));
+	}
 
 }
